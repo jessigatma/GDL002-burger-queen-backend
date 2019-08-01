6d6f9e259440ffe06b102b89ae4455c0b15be434
jessigatma <jessgat@hotmail.com>
2019-07-31 21:45:23 -0500

    se agregaron rutas al archivo now.json

--- index.js
+++ index.js
@@ -9,42 +9,34 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 // const Product = require('./models/Product')
 
-
-const {
-  port,
-  mongoUrl,
-  secret
-} = config;
+const { port, mongoUrl, secret } = config;
 const app = express();
-
 
 // Conectar aplicación a MongoDB
 mongoose.connect(mongoUrl, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
-
 
 app.set('config', config);
 app.set('pkg', pkg);
 
-
 app.use(express.json());
 app.use(authMiddleware(secret));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(cors());
 // app.use(bodyParser.json());
 
-routes(app, (err) => {
+routes(app, err => {
   if (err) {
     throw err;
   }
-
 
   // Registro de "middleware" que maneja posibles errores
   app.use(errorHandler);
 
   app.listen(port, () => console.log(`App listening on port ${port}`));
 });
-
--- routes/orders.js
+++ routes/orders.js
@@ -6,99 +6,102 @@ const {
 } = require('../middleware/auth');
 
 module.exports = (app, next) => {
-
   app.get('/orders', (req, res) => {
-
     Order.find({}, (err, orders) => {
-      if (err) return res.status(500).send({
-        message: 'Error al realizar petición'
-      });
-      if (!orders) return res.status(404).send({
-        message: 'No existe el producto'
-      })
+      if (err)
+        return res.status(500).send({
+          message: 'Error al realizar petición',
+        });
+      if (!orders)
+        return res.status(404).send({
+          message: 'No existe el producto',
+        });
 
       res.status(200).send({
-        orders
-      })
-    })
+        orders,
+      });
+    });
   });
 
-  app.get('/orders/:orderId',  (req, res) => {
-    let orderId = req.params.orderId
+  app.get('/orders/:orderId', (req, res) => {
+    let orderId = req.params.orderId;
 
     Order.findById(orderId, (err, order) => {
-      if (err) return res.status(500).send({
-        message: 'Error al realizar la petición'
-      })
-      if (!order) return res.status(404).send({
-        message: 'No existe el producto'
-      })
+      if (err)
+        return res.status(500).send({
+          message: 'Error al realizar la petición',
+        });
+      if (!order)
+        return res.status(404).send({
+          message: 'No existe el producto',
+        });
 
       res.status(200).send({
-        order
-      })
-    })
+        order,
+      });
+    });
   });
 
   app.post('/orders', (req, res) => {
-    console.log('POST /orders')
-    console.log(req.body)
+    console.log('POST /orders');
+    console.log(req.body);
 
-    let order = new Order()
+    let order = new Order();
 
-    order.cashOrder = req.body.cashOrder
-    order.table = req.body.table
-    order.dated = req.body.dated
+    order.cashOrder = req.body.cashOrder;
+    order.table = req.body.table;
+    order.dated = req.body.dated;
     // order.status = req.body.status
 
     order.save((err, orderStored) => {
-      if (err) res.status(500).send({
-        message: `Error al salvar en la base de datos ${err}`
-      })
+      if (err)
+        res.status(500).send({
+          message: `Error al salvar en la base de datos ${err}`,
+        });
 
       res.status(200).send({
-        order: orderStored
-      })
-    })
+        order: orderStored,
+      });
+    });
   });
 
   app.put('/orders/:orderId', (req, res) => {
-    let orderId = req.params.orderId
-    let update = req.body
+    let orderId = req.params.orderId;
+    let update = req.body;
 
     Order.findByIdAndUpdate(orderId, update, (err, orderUpdated) => {
-      if (err) res.status(500).send({
-        message: `Error al salvar en la base de datos ${err}`
-      })
+      if (err)
+        res.status(500).send({
+          message: `Error al salvar en la base de datos ${err}`,
+        });
 
       res.status(200).send({
-        order: orderUpdated
-      })
-    })
+        order: orderUpdated,
+      });
+    });
   });
 
   app.delete('/orders/:orderId', (req, res) => {
-    let orderId = req.params.orderId
+    let orderId = req.params.orderId;
 
     Order.findById(orderId, (err, order) => {
-      if (err) res.status(500).send({
-        message: `Error al salvar en la base de datos ${err}`
-      })
+      if (err)
+        res.status(500).send({
+          message: `Error al salvar en la base de datos ${err}`,
+        });
 
       order.remove(err => {
-        if (err) res.status(500).send({
-          message: `Error al salvar en la base de datos ${err}`
-        })
+        if (err)
+          res.status(500).send({
+            message: `Error al salvar en la base de datos ${err}`,
+          });
 
         res.status(200).send({
-          message: 'El producto ha sido eliminado'
-        })
-      })
-    })
-  })
-
-
+          message: 'El producto ha sido eliminado',
+        });
+      });
+    });
+  });
 
   return next();
-
-}
+};
