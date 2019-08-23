424115ef8c76c3e7699c6d6fa80d061c0c3792c6
jessigatma <jessgat@hotmail.com>
2019-08-22 22:55:19 -0500

    cambios en package json

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.mongoUrl = process.env.MONGO_URL || 'mongodb+srv://jessgatma:jessicagm@cluster0-2stiw.mongodb.net/test?retryWrites=true&w=majority';
+exports.mongoUrl =
+  process.env.MONGO_URL ||
+  'mongodb+srv://jessgatma:jessicagm@cluster0-2stiw.mongodb.net/test?retryWrites=true&w=majority';
 exports.secret = process.env.JWT_SECRET || 'xxxxxxxx'; // JWT secret
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
